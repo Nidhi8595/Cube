1071a2164a07d4df7cbca7aec6cb057bf1450583
Neelakshi <neelakshikadyan@gmail.com>
2026-01-03 18:40:10 +0530

    caps navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ export default function Navbar() {
   return (
     <header className="bg-cube-navy text-white sticky top-0 z-50 border-b border-white/5">
 
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-8 h-20">
-        <div className="font-bold text-xl">CUBE</div>
+      <div className="max-w-7xl mx-auto flex items-center justify-between pr-8 h-20">
+        <div className="font-bold text-2xl">CUBE</div>
 
         <nav className="flex gap-8 text-lg text-gray-200">
-          {["Platform", "Products", "Customers", "Resources", "Pricing"].map(i => (
+          {["PLATFORM", "PRODUCTS", "CUSTOMERS", "RESOURCES", "PRICING","REQUESTED DEMO","SIGN IN"].map(i => (
             <span
               key={i}
               className="relative cursor-pointer after:absolute after:-bottom-1 after:left-0 after:h-[1px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full"
